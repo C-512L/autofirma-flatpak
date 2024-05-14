4805832cc63b6d6b8cc712257ea5a8ccef098985
Vicente Maroto Garzón <vicente.maroto.garzon@c512l.dev>
2024-05-14 18:56:32 +0200

    Use a more proper directory structure inside /app

--- install-autofirma.sh
+++ install-autofirma.sh
@@ -4,12 +4,12 @@ shopt -s failglob
 
 mkdir deb-package
 ar p  AutoFirma_1*.deb data.tar.gz | tar zxvf - -C deb-package
-sed -i "s/ \/usr\// \/app\/usr\//" deb-package/usr/bin/autofirma
+sed -i "s/ \/usr\// \/app\//" deb-package/usr/bin/autofirma
 
-install -Dm755 deb-package/usr/bin/autofirma                                /app/usr/bin/autofirma
-install -Dm644 deb-package/usr/lib/AutoFirma/AutoFirma.jar                  /app/usr/lib/AutoFirma/AutoFirma.jar
-install -Dm644 deb-package/usr/lib/AutoFirma/AutoFirma.png                  /app/usr/lib/AutoFirma/AutoFirma.png
-install -Dm644 deb-package/usr/lib/AutoFirma/AutoFirmaConfigurador.jar      /app/usr/lib/AutoFirma/AutoFirmaConfigurador.jar
+install -Dm755 deb-package/usr/bin/autofirma                                /app/bin/autofirma
+install -Dm644 deb-package/usr/lib/AutoFirma/AutoFirma.jar                  /app/lib/AutoFirma/AutoFirma.jar
+install -Dm644 deb-package/usr/lib/AutoFirma/AutoFirma.png                  /app/lib/AutoFirma/AutoFirma.png
+install -Dm644 deb-package/usr/lib/AutoFirma/AutoFirmaConfigurador.jar      /app/lib/AutoFirma/AutoFirmaConfigurador.jar
 
 mkdir -p /app/share/icons/hicolor/scalable/
 install -Dm644 AutoFirma.svg                                                /app/share/icons/hicolor/scalable/com.github.aruiz.Autofirma.svg
