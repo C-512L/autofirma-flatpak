dc75d979ae2bf7731bc15a31447a07d2f6b82173
Vicente Maroto Garzón <vicente.maroto.garzon@c512l.dev>
2024-05-20 21:45:08 +0200

    Do not ship AutoFirmaConfigurador.jar It's completely broken inside the runtime

--- install-autofirma.sh
+++ install-autofirma.sh
@@ -9,6 +9,5 @@ sed -i "s/ \/usr\// \/app\//" deb-package/usr/bin/autofirma
 install -Dm755 afirma-wrapper.sh                                            /app/bin/afirma-wrapper
 install -Dm755 deb-package/usr/bin/autofirma                                /app/bin/autofirma
 install -Dm644 deb-package/usr/lib/AutoFirma/AutoFirma.jar                  /app/lib/AutoFirma/AutoFirma.jar
-install -Dm644 deb-package/usr/lib/AutoFirma/AutoFirmaConfigurador.jar      /app/lib/AutoFirma/AutoFirmaConfigurador.jar
 install -Dm644 AutoFirma.svg                                                "/app/share/icons/hicolor/scalable/apps/afirma.svg"
 install -Dm644 "afirma.desktop"                                             "/app/share/applications/afirma.desktop"
